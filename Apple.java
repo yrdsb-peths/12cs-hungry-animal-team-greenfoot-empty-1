6170ea34afee82e3f4332766f6d1dbc540383fdf
TheLostone145 <martinthegamer145@gmail.com>
2024-09-25 12:47:14 -0400

    Apple mod

--- Apple.java
+++ Apple.java
@@ -1,21 +1,13 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Apple here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class Apple extends Actor
 {
-    /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private int speed=1;
     public void act()
     {
         int x= getX();
-        int y = getY()+2;
+        int y = getY()+speed;
         setLocation(x, y);
         
         MyWorld world= (MyWorld) getWorld();
@@ -23,5 +15,10 @@ public class Apple extends Actor
             world.gameOverCode();
             world.removeObject(this);
         }
+        
+    }
+    public void setSpeed(int spd)
+    {
+        speed=spd;
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -3,6 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class MyWorld extends World
 {
     public int score=0;
+    int level;
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -23,7 +24,10 @@ public class MyWorld extends World
         addObject(gameOverLabel, 300, 200);
     }
     public void increaseScore(){
-        score++; 
+        score++;
+        if(score % 5 ==0){
+            level++;
+        }
     }
     public void createApple()
     {
